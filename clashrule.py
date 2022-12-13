67fa7f8f82789431c08dc52ce50765da037e7821
Canoziia <2433648251@qq.com>
2022-12-13 19:57:17 +0800

    change

--- clashrule.py
+++ clashrule.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
 import ruamel.yaml
 import sys
+import requests
 
 
 yaml = ruamel.yaml.YAML()
 
-with open("./Config/client/client.yaml") as f:
-    client = yaml.load(f)
-with open("./Config/proxy-providers/All.yaml") as f:
-    proxy_roviders = yaml.load(f)
-with open("./Config/rule-providers/ACL4SSR.yaml") as f:
-    rule_providers = yaml.load(f)
-with open("./Config/rule-groups/ACL4SSR_Full.yaml") as f:
-    rule_groups = yaml.load(f)
-
-# 如果own有值, 则取own, 如果own没有值或值为空, 则取default
+githubraw = "https://raw.lovely.ink"
 
 
 def merge(own: dict, default: dict):
+    # 如果own有值, 则取own, 如果own没有值或值为空, 则取default
     for key in default:
         if (key not in own) or not own[key]:
             own[key] = default[key]
@@ -26,33 +19,42 @@ def merge(own: dict, default: dict):
     return own
 
 
-merge(client, proxy_roviders)
-merge(client, rule_providers)
-merge(client, rule_groups)
-
-
 def generate(url=""):
-    client["proxy-providers-template"]["url"] = url
-    with open("./Config/ACL4SSR_Online_Full.yaml", "w") as f:
-        yaml.dump(client, f)
+    with open("./Config/client/client.yaml") as f:
+        client = yaml.load(f)
+    with open("./Config/proxy-providers/All.yaml") as f:
+        proxy_roviders = yaml.load(f)
+    with open("./Config/rule-providers/ACL4SSR.yaml") as f:
+        rule_providers = yaml.load(f)
+    with open("./Config/rule-groups/ACL4SSR_Full.yaml") as f:
+        rule_groups = yaml.load(f)
 
-# 使用flask, 接受get的provider参数, 并将client["proxy-providers-template"]["url"]赋值为provider, 再返回yaml文件给请求者
+    config = client  # 指针引用
+    merge(config, proxy_roviders)
+    merge(config, rule_providers)
+    merge(config, rule_groups)
+    config["proxy-providers-template"]["url"] = url
+    with open("./Config/ACL4SSR_Online_Full.yaml", "w") as f:
+        yaml.dump(config, f)
 
 
 def listen(port=80):
+
+    # 使用flask, 接受get的provider参数, 并将client["proxy-providers-template"]["url"]赋值为provider, 再返回yaml文件给请求者
     import io
     from flask import Flask, request, Response
     app = Flask(__name__)
 
     @app.route("/")
     def index():
+        resp = requests.get(
+            githubraw+"/darknightlab/ClashRule/main/Config/ACL4SSR_Online_Full.yaml")
+        config = yaml.load(resp.text)
         buf = io.BytesIO()
         provider = request.args.get("provider")
         if provider:
-            client["proxy-providers-template"]["url"] = provider
-        else:
-            client["proxy-providers-template"]["url"] = ""
-        yaml.dump(client, buf)
+            config["proxy-providers-template"]["url"] = provider
+        yaml.dump(config, buf)
         return Response(buf.getvalue(), mimetype="text/plain")
 
     app.run(host="0.0.0.0", port=port)
